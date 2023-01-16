2a3d1c041cbd8c077e40809f9a6d65a9bc2039cf
MuriloVieira <murilo@pizzamiglio.eti.br>
2023-01-16 15:19:34 -0300

    Curso finalizado

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["../**/*.html"],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         'new-blue': '#243C5A',
+        amber: colors.amber,
       },
       spacing: {
         '100' : '25rem',
